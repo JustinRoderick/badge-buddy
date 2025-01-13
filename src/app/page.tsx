748a58c1fbabc0ed5cdbfa852581f8152955cf6f
JustinRoderick <justinroderick258@gmail.com>
2025-01-12 23:59:04 -0500

    update some meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-3xl">Badge Buddy</h1>
+          <p className="text-lg">Coming Soon Gang</p>
         </div>
       </main>
     </HydrateClient>
